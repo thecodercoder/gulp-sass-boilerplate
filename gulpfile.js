4709d779fec11445d0e7a3c7080340ad710c99a7
thecodercoder <37986728+thecodercoder@users.noreply.github.com>
2021-07-27 16:20:40 -0500

    Remove modules used for JS

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,6 @@ const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-const babel = require('gulp-babel');
-const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
 
 // Sass Task
@@ -13,14 +11,6 @@ function scssTask() {
 	return src('app/scss/style.scss', { sourcemaps: true })
 		.pipe(sass())
 		.pipe(postcss([autoprefixer(), cssnano()]))
-		.pipe(dest('dist', { sourcemaps: '.' }));
-}
-
-// JavaScript Task
-function jsTask() {
-	return src('app/js/script.js', { sourcemaps: true })
-		.pipe(babel({ presets: ['@babel/preset-env'] }))
-		.pipe(terser())
 		.pipe(dest('dist', { sourcemaps: '.' }));
 }
 
